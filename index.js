951edc3ce2b91b4287bd1b587216ae684442e5f8
samclarkb <74268703+samclarkb@users.noreply.github.com>
2022-03-22 12:21:42 +0100

    render home page

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
 
 app.get(`/`, async (req, res) => {
-	res.send('hello world')
+	res.render('home')
 })
 
-app.listen(3000)
+app.use((req, res) => {
+	res.status(404).send(':(')
+})
+
+app.listen(port, () => {
+	console.log(`Listening on port: ${port}`)
+})
