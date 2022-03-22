026082d76a916c004b74f534c3ddd36026e281e3
samclarkb <74268703+samclarkb@users.noreply.github.com>
2022-03-22 13:14:20 +0100

    instant redirect results page

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express')
+const fetch = require('node-fetch')
 const app = express()
 const port = 3000
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
-app.get(`/`, async (req, res) => {
+app.get(`/`, (req, res) => {
 	res.render('home')
+})
+
+app.get(`/results`, async (req, res) => {
+	await fetch(`https://world.openfoodfacts.org/api/v0/product/${req.query.query}.json`)
+		.then(res => res.json())
+		.then(data => {
+			if (data == 1) {
+				res.render('/results', { product: data.product })
+			} else {
+				res.redirect('/home')
+			}
+		})
 })
 
 app.use((req, res) => {
