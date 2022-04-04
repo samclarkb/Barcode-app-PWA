b096cde14d319057b1d89932c8eb0d5ec14345ba
samclarkb <74268703+samclarkb@users.noreply.github.com>
2022-04-04 22:44:54 +0200

    offline mode is working

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,23 +1,16 @@
-// const CORE_CACHE_VERSION = 'v3'
-// const CORE_ASSETS = ['/offline', 'styles/styles.css', '/index.js']
+const staticCache = 'static'
+const assets = ['/', '/styles/style.css'] // Stuff I want in the cache
 
-// self.addEventListener('install', event => {
-// 	console.log('installing cache')
-
-// 	event.waitUntil(
-// 		caches.open(CORE_CACHE_VERSION).then(function (cache) {
-// 			return cache.addAll(CORE_ASSETS).then(() => self.skipWaiting())
-// 		})
-// 	)
-// })
-
-// self.addEventListener('activate', event => {
-// 	// event.waitUntil(clients.claim());
-// })
-
-// installing service worker
+// Acts on the install event.
 self.addEventListener('install', evt => {
 	console.log('service worker has been installed')
+	// When the service worker get installed, the service worker will put the assets in the cache for future events
+	evt.waitUntil(
+		// Basically waits until this function is done. after that the install event will occur
+		caches.open(staticCache).then(cache => {
+			cache.addAll(assets)
+		})
+	)
 })
 
 // Activating service worker
@@ -25,7 +18,13 @@ self.addEventListener('activate', evt => {
 	console.log('service worker has been activated')
 })
 
-// fetch
+// Every time a fetch occur, this function will run
 self.addEventListener('fetch', evt => {
 	console.log('fetch', evt)
+
+	evt.respondWith(
+		caches.match(evt.request).then(cacheRes => {
+			return cacheRes || fetch(evt.request)
+		})
+	)
 })
