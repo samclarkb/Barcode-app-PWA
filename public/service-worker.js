462f1488029b696bcdfadd22b2a9741eab7701c6
samclarkb <74268703+samclarkb@users.noreply.github.com>
2022-04-08 12:47:07 +0200

    refresh function is broken

--- public/service-worker.js
+++ public/service-worker.js
@@ -16,21 +16,14 @@ self.addEventListener('install', evt => {
 
 // activate event
 self.addEventListener('activate', evt => {
-	evt.waitUntil(
-		caches.keys().then(keys => {
-			return Promise.all(
-				keys
-					.filter(key => key !== staticCache && key !== dynamicCache)
-					.map(key => caches.delete(key))
-			)
-		})
-	)
+	evt.waitUntil()
 })
 
 // Every time a fetch occur, this function will run
 self.addEventListener('fetch', evt => {
 	// if the cache contains something with evt.request than return it from the cache instead of the server.
 	evt.respondWith(
+		// if the request is in one of the caches, return cache
 		caches
 			.match(evt.request)
 			.then(cacheRes => {
@@ -39,12 +32,12 @@ self.addEventListener('fetch', evt => {
 					fetch(evt.request).then(fetchRes => {
 						// If the cache is empty, than run the original fetch from the server.
 						return caches.open(dynamicCache).then(cache => {
-							cache.put(evt.request.url, fetchRes.clone())
+							cache.put(evt.request.url, fetchRes.clone()) //puts the requested url in the
 							return fetchRes
 						})
 					})
 				)
 			})
-			.catch(() => caches.match('/offline'))
+			.catch(() => caches.match('/offline')) // When user requests a page thats not in the cache, the user wil be directed to the offline page
 	)
 })
